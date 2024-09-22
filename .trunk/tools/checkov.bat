cfa5ef3d7985c1625b2b33eb88400f6efc524eb6
Aleks <aleksbogdanovazure@gmail.com>
2024-09-22 23:50:07 +0300

    ADding files

---
+++ .trunk/tools/checkov.bat
@@ -0,0 +1,3 @@
+@echo off
+echo There was an error while installing checkov. See .trunk/out/IHpuw.yaml for details.
+exit /b 1;
---
+++ .trunk/tools/shellcheck.bat
@@ -0,0 +1,3 @@
+@echo off
+echo There was an error while installing shellcheck. See .trunk/out/p8G0X.yaml for details.
+exit /b 1;
